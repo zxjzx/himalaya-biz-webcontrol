6d2b315df3102bda049609300b6ae8b52a835a74
l李盼 <@yijiupi.cn>
2017-07-19 10:12:03 +0800

    multiJiupiCityTree指令全选修改！

--- src/main/resources/static/multiJiupiCityTree/multiJiupiCityTree.js
+++ src/main/resources/static/multiJiupiCityTree/multiJiupiCityTree.js
@@ -87,32 +87,34 @@
 	        	
 		        	//初始化选中的值（一般用于编辑）
 	                function initNodes(nodeList){
-	                	//获取所有的cityId集合
-	                	var AllCityIdList = [] ; 
-	                	//默认为false ;
-	                	$scope.checked = false ;
+	                	//全选默认为true ;
+	                	$scope.checked = true ;
+	                	if(nodeList.length){
+	                		angular.forEach($scope.zNodes,function(parentItem){
+		                		angular.forEach(parentItem.children,function(childItem){
+		                			angular.forEach(nodeList,function(son){
+		                				//初始化勾选已存在的值
+			                    		if(son == childItem.id){
+			                    			parentItem.open = true ;
+			                    			parentItem.checked = true ;
+			                    			childItem.checked = true;
+			                    			if($scope.isDisabled){
+			                    				parentItem.chkDisabled = true ;
+			                    				childItem.chkDisabled = true ;
+			                    			}
+			                    		}
+			                    	});
+			                	})
+		                	})
+	                	}
+	                	//遍历所有的节点，看是否有没有勾选中的,若有,则取消全选
 	                	angular.forEach($scope.zNodes,function(parentItem){
 	                		angular.forEach(parentItem.children,function(childItem){
-	                			//遍历所有的节点，获取所有的cityId
-	                			AllCityIdList.push(childItem.id);
-	                			angular.forEach(nodeList,function(son){
-	                				//初始化勾选已存在的值
-		                    		if(son == childItem.id){
-		                    			parentItem.open = true ;
-		                    			parentItem.checked = true ;
-		                    			childItem.checked = true;
-		                    			if($scope.isDisabled){
-		                    				parentItem.chkDisabled = true ;
-		                    				childItem.chkDisabled = true ;
-		                    			}
-		                    		}
-		                    	});
+	                			if(!childItem.checked){
+	                				$scope.checked = false ;
+	                			}
 		                	})
 	                	})
-	                	//将遍历获取的所有cityId与已存在的cityId做比较,数量相同，则证明全选中
-	                	if(AllCityIdList.length == nodeList.length){
-	                		$scope.checked = true ;
-	                	}
 	                }
 	                
 	                function showNode(){
