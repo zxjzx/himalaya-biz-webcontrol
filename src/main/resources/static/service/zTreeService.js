abc316eb82c951015844e3bb4c18e9e59dc73415
l李盼 <@yijiupi.cn>
2017-07-31 10:48:50 +0800

    multiJiupiCity添加getJiupiCityObjList的外部方法。

--- src/main/resources/static/service/zTreeService.js
+++ src/main/resources/static/service/zTreeService.js
@@ -10,12 +10,17 @@
 		//存储用户已选酒批城市Id节点列表
 	    var cacheJiupiCityIdList = [];
 	    
+	    //获得所有的酒批城市对象
+	    var allJiupiCityObjList ;
+	    //选中的酒批城市对象集合
+	    var checkedJiupiCityObjList = [] ;
+	    
 	    /**
 	     * 缓存用户已选节点 
 	     */
 	    this.cahceUserCheckedNode = function(checkedNodeList,notCheckedNodeList){
 	    	
-	    	//还没有任何缓存时，直接将用户已选中节点存入allCheckedNodeList
+	    	//还没有任何缓存时，直接将用户已选中节点存入cacheJiupiCityIdList
 	    	if(!cacheJiupiCityIdList || cacheJiupiCityIdList.length == 0){
 	    		angular.forEach(checkedNodeList,function(newCheckedNode){
 	    			if(!newCheckedNode.isParent){
@@ -45,60 +50,36 @@
 	    		})
 	    		
 	    	}
-	    }
-
-	    /**
-	     * 通过当前节点递归获取父节点
-	     */
-	    function getParentNodeByNode(nodeList, node) {
-	        if (node) {
-	            nodeList.push(node);
-	            getParentNodeByNode(nodeList, node.getParentNode());
-	        }
-	    }
-
-	    /**
-	     * 获取叶子节点的全路径名称
-	     */
-	    function getFullNameByNodeId(id) {
-	        var nodeList = [];
-	        var fullName = "";
-	        var currentNode = getTreeNodeById(id);
-	        getParentNodeByNode(nodeList, currentNode);
-	        for (var i = nodeList.length - 1; i >= 0; i--) {
-	            fullName += nodeList[i].name;
-	        }
-	        return fullName;
-
+	    	
+	    	//获取选中的酒批城市对象集合
+	    	angular.forEach(allJiupiCityObjList,function(parentItem){
+	    		angular.forEach(parentItem.children,function(childItem){
+	    			angular.forEach(cacheJiupiCityIdList,function(cityIdItem){
+	    	    		if(cityIdItem == childItem.id){
+	    	    			checkedJiupiCityObjList.push(childItem);
+	    	    		}
+	    	    	})
+	    		})
+	    	})
 	    }
 	    
-	    /**
-	     * 获取选中的jiupiCityObject集合
-	     */
-	    this.getJiupiCityObjList = function () {
-	        var leafNodes = [];
-	        angular.forEach(allCheckedNodeList, function (item) {
-	            if (!item.isParent) {
-	                var fullName = getFullNameByNodeId(item.id);
-	                //父节点
-	                var pNode = item.getParentNode();
-	                leafNodes.push({
-	                    cityId: item.id,
-	                    province:pNode.name ,
-	                    city: item.name,
-	                    fullName: fullName,
-	                });
-	            }
-	        });
-	        return leafNodes;
-	    };
-	    
+	    //储存所有的酒批城市
+	    this.setAllJiupiCity = function(allTreeNode){
+	    	allJiupiCityObjList = allTreeNode ;
+	    }
 	    
 	    /**
 	     * 获取选中的jiupiCityId集合 
 	     */
 	    this.getJiupiCityIdList = function () {
 	        return cacheJiupiCityIdList;
+	    }
+	    
+	    /**
+	     * 获取选中的jiupiCityObj集合 
+	     */
+	    this.getJiupiCityObjList = function(){
+	    	return checkedJiupiCityObjList ;
 	    }
 	}]);
 	
