1832226e0bdb9c90c6292bb05b189538332107f5
l李盼 <@yijiupi.cn>
2017-08-03 15:45:28 +0800

    ejpJiupiCitySelect指令修改。

--- src/main/resources/static/jiupiCitySelect/ejpJiupiCitySelect.js
+++ src/main/resources/static/jiupiCitySelect/ejpJiupiCitySelect.js
@@ -2,27 +2,109 @@
  * 酒批城市选择控件
  */
 (function(){
-	'use strict' ;
+	'use strict';
 	
 	angular.module("ejpJiupiCitySelectModule",[])
+	/**
+	 * 酒批城市指令 
+	 */
 	.directive("ejpJiupiCitySelect",[function(){
 		return {
 			restrict:'E',
 			scope:{
-				jiupicityVo:'=',
+				jiupiCityVo : '=' ,
+				hidePleaseSelect :'=' ,
+				changeJiupiProvince : '=' ,
+				changeJiupiCity : '=' ,
 			},
 			templateUrl:'../static/jiupiCitySelect/ejpJiupiCitySelect.html',
-			controller:["$scope","$http",function($scope,$http){
+			controller:["$scope","$http",'ejpJiupiCityService',function($scope,$http,ejpJiupiCityService){
 				
-				//获取酒批城市
-				$http.get("/bizwebcontrol/jiupicitymultitree/listProvinceAllCitiesDTO/" + -1)
-					.success(function(data){
-					if(data.result==='success'){
-						console.log(data);
+				$scope.jiupiCityVo.provinceObj = {} ;
+				
+				// 获取酒批城市
+				var getJiupiCity = function(){
+					ejpJiupiCityService.listProvinceAllCitiesDTO(-1).then(function(data){
+						if(data.result==='success'){
+							$scope.allJiupiCityList = data.list ; 
+							ejpJiupiCityService.cacheJiupiCityData($scope.allJiupiCityList);
+							initJiupiCity();
+						}
+					})
+				}
+				getJiupiCity();
+				
+				//初始化酒批城市
+				var initJiupiCity = function(){
+					if($scope.jiupiCityVo.cityId){
+						angular.forEach($scope.allJiupiCityList,function(provinceItem){
+							angular.forEach(provinceItem.jiupiCityList,function(cityItem){
+								if($scope.jiupiCityVo.cityId == cityItem.id){
+									$scope.jiupiCityVo.provinceObj.province = cityItem.province ;
+									$scope.jiupiCityVo.provinceObj.jiupiCityList = provinceItem.jiupiCityList ;
+								}
+							})
+						})
 					}
-				})
+				}
 			}]
+		 }	
+	}])
+	
+	/**
+	 * 酒批城市服务
+	 */
+	.service("ejpJiupiCityService",['$http','$q',function($http,$q){
+		
+		this.listProvinceAllCitiesDTO = function(cityMode){
+			   return $q(function (resolve, reject) {
+		           $http({
+		               method: 'get',
+		               url: 'bizwebcontrol/jiupicitymultitree/listProvinceAllCitiesDTO/' + cityMode,
+		           }).success(function (data) {
+		               resolve(data)
+		           }).error(function (data) {
+		               reject(data.message);
+		           });
+		       });
+		   }
+		
+		var cacheJiupiCityList = [] ;
+		//缓存酒批数据
+		this.cacheJiupiCityData = function(allJiupiCityList){
+			cacheJiupiCityList = allJiupiCityList ;
 		}
 		
+		//根据酒批cityId匹配对应的酒批数据对象
+		this.getJiupiCityObjByCityId = function(cityId){
+			var jiupiCityObj = {} ;
+			jiupiCityObj.provinceObj = {} ;
+			angular.forEach(cacheJiupiCityList,function(provinceItem){
+				angular.forEach(provinceItem.jiupiCityList,function(cityItem){
+					if(cityId == cityItem.id){
+						jiupiCityObj.provinceObj.province = provinceItem.province ;
+						jiupiCityObj.provinceObj.jiupiCityList = provinceItem.jiupiCityList ;
+						jiupiCityObj.cityId = cityId ;
+					}
+				})
+			})
+			return jiupiCityObj ;
+		} 
+		
+		//根据CityId获取选中的cityObj
+		this.getCityObjByCityId = function(cityId){
+			var cityObj = {} ;
+			angular.forEach(cacheJiupiCityList,function(provinceItem){
+				angular.forEach(provinceItem.jiupiCityList,function(cityItem){
+					if(cityId == cityItem.id){
+						cityObj = cityItem ;
+					}
+				})
+			})
+			return cityObj ;
+		}
+		
+		
 	}])
+	
 }());
