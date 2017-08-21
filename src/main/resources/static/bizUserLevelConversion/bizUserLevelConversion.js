17c2974abd482ba258fef69f2e75a465cf84b5f3
lipan <lipan@JAVADEVELOPER44.yjplan.net>
2017-08-21 11:33:53 +0800

    bizUserLevelDropdown,会员等级下拉框指令。

--- src/main/resources/static/bizUserLevelConversion/bizUserLevelConversion.js
+++ src/main/resources/static/bizUserLevelConversion/bizUserLevelConversion.js
@@ -19,6 +19,7 @@
 			controller:['$scope','$http','BizUserLevelService',function($scope,$http,BizUserLevelService){
 				BizUserLevelService.getBizUseLevelList().then(function(data){
 					$scope.bizUserLevelList = angular.copy(data.list);
+					BizUserLevelService.cacheBizUserLevelData($scope.bizUserLevelList);
 				},function(data){
 					console.info(data.detailMessage);
 				});
@@ -27,6 +28,50 @@
 		};
 	}])
 	
+	//获取会员等级的下拉框指令（多用于促销活动中）
+	.directive('bizUserLevelDropdown',[function(){
+		return {
+			restrict: 'E',
+			scope:{
+				userLevelId : '=' , //会员等级vo对象，
+				changeUserLevel : '&' , //改变会员等级胡触发函数 ，
+				isDisabled : '=' , //是否禁用会员等级下拉框
+			},
+			templateUrl : '../static/bizUserLevelConversion/bizUserLevelDropdown.html',
+			replace:true,
+			controller:['$scope','$http','BizUserLevelService',function($scope,$http,BizUserLevelService){
+				
+				//初始化会员等级下拉框列表
+				$scope.userLevelList = [] ;
+				
+				var getBizUserLevelList = function(){
+					BizUserLevelService.getBizUseLevelList().then(function(data){
+						console.log(data);
+						var bizUserLevelList = angular.copy(data.list);
+						//缓存数据
+						BizUserLevelService.cacheBizUserLevelData(bizUserLevelList);
+						for(var i=0 ; i<bizUserLevelList.length ; i++){
+							var item = bizUserLevelList[i] ;
+							if(i == bizUserLevelList.length-1){
+								item.name = item.levelName ;
+								$scope.userLevelList.push(item) ;
+							}else {
+								item.name = item.levelName + "会员及以上" ;
+								$scope.userLevelList.push(item) ;
+							}
+						}
+					}).then(function(){
+						if($scope.userLevelId==undefined || $scope.userLevelId==""){
+							//初始化会员等级为下拉框第一个会员等级
+							$scope.userLevelId = $scope.userLevelList[0].id ;
+						}
+					})
+				}
+				getBizUserLevelList() ;
+				
+			}]
+		};
+	}])
 	
 	//获取会员等级接口服务
 	.service('BizUserLevelService', ['$http', '$q', function ($http, $q) {
@@ -53,5 +98,23 @@
             });
         });
     }
+    
+    var cacheBizUserLevelList = [] ;
+    //缓存会员等级列表数据
+    this.cacheBizUserLevelData = function(bizUserLevelList){
+    	cacheBizUserLevelList = bizUserLevelList ;
+    }
+    
+    //根据会员等级Id获取会员等级对象
+    this.getUserLevlObjById = function(id){
+    	var userLevelObj = {} ;
+    	angular.forEach(cacheBizUserLevelList,function(item){
+    		if(item.id == id){
+    			userLevelObj = item ;
+    		}
+    	})
+    	return userLevelObj ;
+    }
+    
   }])
 }());
