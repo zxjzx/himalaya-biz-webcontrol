cfdc36a7a36046785bde365b26abcf2fc6ce6159
lipan <lipan@JAVADEVELOPER44.yjplan.net>
2017-08-23 17:13:32 +0800

    产品展示类目回显优化！

--- src/main/resources/static/category/productDisplayCategory/productDisplayCategory.js
+++ src/main/resources/static/category/productDisplayCategory/productDisplayCategory.js
@@ -23,27 +23,46 @@
 				displayCategoryVo : '=' , 
 				displayPartnerCategoryId : '=' ,         //展示类目的父类目Id
 				displaySubcategoryId : '=' ,	         //展示类目的子类目Id
+				bizUserDisplayClassCode : '=' ,	         //会员展示类别code
+				displayCategoryId : '=' ,	         	 //展示类目Id(一般针对只有一个类目Id,不清楚是一级类目还是二级类目的情况)
 				changeBizUserDisplayClass : '&' , 		 //外部函数，选择会员展示类别的触发函数
 				changeDisplayPartnerCategory : '&' ,     //外部函数，选择父类目的触发函数
 				changeDisplaySubcategory : '&'  	     //外部函数，选择子类目的触发函数
 			},
 			templateUrl : '../static/category/productDisplayCategory/productDisplayCategory.html',
-			controller : ['$http','$scope','productCategoryService','findDictionaryByCodeService',
-			              function($http,$scope,productCategoryService,findDictionaryByCodeService){
+			controller : ['$http','$scope','findDictionaryByCodeService','productDisplayCategoryService',
+			              function($http,$scope,findDictionaryByCodeService,productDisplayCategoryService){
 				
 				//获取会员展示类别
 				var getBizUserDisplayClassList = function(){
 					findDictionaryByCodeService.getDictionaryTypeDetailByCode("BizUserDisplayClass").then(function(data){
 						$scope.bizUserDisplayClassList = data.data.items;
+						productDisplayCategoryService.cacheBizUserDisplayClassData(bizUserDisplayClassList);
+						//回显会员展示类别
+						if($scope.bizUserDisplayClassCode){
+							$scope.displayCategoryVo = productDisplayCategoryService.getBizUseDisplayClassObjByCode($scope.bizUserDisplayClassCode);
+							findProductDisplayCategoryList($scope.bizUserDisplayClassCode);
+						}
 					})
 				}
 				getBizUserDisplayClassList();
 				
 				//获取产品展示类目列表
 				var findProductDisplayCategoryList = function(bizUserDisplayClass){
-					productCategoryService.listDisplayCategoryWithChildNoBrand(bizUserDisplayClass).then(function(data){
+					productDisplayCategoryService.listDisplayCategoryWithChildNoBrand(bizUserDisplayClass).then(function(data){
 						if(data.result==='success'){
 							$scope.displayCategoryVo.allDisplayCategoryList = data.list ;
+							productDisplayCategoryService.cacheAllDisplayCategoryData($scope.displayCategoryVo.allDisplayCategoryList);
+							//展示类目回显
+							if($scope.displayCategoryId){
+								$scope.displayCategoryVo = productDisplayCategoryService.getCategoryObjByCategoryId($scope.displayCategoryId);
+							}else {
+								if($scope.displayPartnerCategoryId && !$scope.displaySubcategoryId){
+									$scope.displayCategoryVo = productCategoryService.getCategoryObjByPartnerCategoryId($scope.displayPartnerCategoryId);
+								}else if($scope.displayPartnerCategoryId && $scope.displaySubcategoryId){
+									$scope.displayCategoryVo = productCategoryService.getCategoryObjBySubcategoryId($scope.displaySubcategoryId);
+								}
+							}
 						}
 					})
 				}
