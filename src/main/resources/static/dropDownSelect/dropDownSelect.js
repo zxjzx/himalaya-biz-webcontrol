abfc9eb39c872dedfea11f50af5006cfb139048e
张小娟 <15527436800@qq.com>
2017-09-15 15:54:21 +0800

    下拉框组件

---
+++ src/main/resources/static/dropDownSelect/dropDownSelect.js
@@ -0,0 +1,32 @@
+/*下拉框枚举*/
+(function () {
+	'use strict';
+	var dropDownSelectModule = angular.module("dropDownSelectModule",[]);
+	dropDownSelectModule.directive("dropDownSelect",[function(){
+		return{
+			restrict:'E',
+			scope:{
+				labelName:'@',
+				labCol:'@',
+				selCol:'@',
+				isShow:'@',
+				ngModel:'=',
+				typeCode:'@'
+			},
+			templateUrl:'../static/dropDownSelect/dropDownSelect.html',
+			controller:['$scope','$http',function($scope,$http){
+				$scope.dropDownSelect=function(){
+					 $http.get('/bizwebcontrol/dictionary/getDictionaryType/' + $scope.typeCode + '.action').success(function(response){
+		                	if(response.result=='success'){
+		                		$scope.dropDownSelectList=response.data.items;
+		                	}
+		                })
+					
+				}
+				$scope.dropDownSelect();
+			}]
+				
+			
+		}
+	}])
+}()) 
