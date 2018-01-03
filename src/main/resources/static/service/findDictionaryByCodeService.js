d2b72caefc8617f7370c9253bcd32cbc53f4e371
张小娟 <15527436800@qq.com>
2018-01-03 10:39:56 +0800

    减少获取字典的http请求

--- src/main/resources/static/service/findDictionaryByCodeService.js
+++ src/main/resources/static/service/findDictionaryByCodeService.js
@@ -17,6 +17,47 @@
 	               reject(data.message);
 	           });
 	       });
-	   }
+	   };
+
+        var self = this;
+        this.dataService = {
+            // 存储所以查询页的查询条件数据，手动刷新页面后数据会清空。
+            codeQueryList:[],
+            codeResultList:{}
+        };
+
+	   this.storeDictionaryDataList = function (dictionaryQueryDataList) {
+
+           var d = $q.defer();
+
+	   		var filterDictionaryQueryDataList = [];
+           dictionaryQueryDataList.forEach(function (item) {
+               if(self.dataService.codeQueryList.indexOf(item) == -1){
+                   filterDictionaryQueryDataList.push(item);
+                   self.dataService.codeQueryList.push(item);
+               }
+           });
+           if(filterDictionaryQueryDataList.length > 0){
+               $http.post('common/queryDictionaryTypeMapByCodeList',filterDictionaryQueryDataList).success(function (response) {
+                   for(var i in response.data){
+                       var itemName = i + "List";
+                       self.dataService.codeResultList[itemName] = response.data[i];
+
+                   }
+                   //根据传入的参数返回相关list
+                   var returnDataList = {};
+                   dictionaryQueryDataList.forEach(function (item) {
+					   var itemName = item+"List";
+					   if(self.dataService.codeResultList[itemName]){
+                           returnDataList[itemName]=self.dataService.codeResultList[itemName].items;
+					   }
+                   })
+                   d.resolve(returnDataList);
+               }).error(function(){
+                   d.reject("error");
+               });
+               return d.promise;
+		   }
+       };
 	}]);
 }());
